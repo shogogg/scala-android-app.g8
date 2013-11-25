91d457171a55f799b721bbe5eda468056575195d
shogo <shogo@studiofly.net>
2013-11-25 18:53:51 +0900

    build.scala にコメントを追加したりした

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -4,41 +4,71 @@ import sbtandroid.AndroidPlugin._
 
 object General {
 
+  // ======================================================================
+  // 基本的な設定
+  // ======================================================================
   val baseSettings: Seq[Project.Setting[_]] = Seq(
-    name            := "$name$",
-    version         := "$version$",
-    versionCode     := 0,
-    scalaVersion    := "$scalaVersion$",
-    platformName    := "android-$targetSdkVersion$",
-    keyalias        := "$keyalias$",
-    useProguard     := true
+    // アプリの名前
+    name := "$name$",
+    // アプリのバージョン
+    version := "$version$",
+    versionCode := "$versionCode$",
+    // アプリのビルドに使用する Scala のバージョン
+    scalaVersion := "$scalaVersion$",
+    // ビルド対象のプラットフォーム（targetSdkVersion）
+    platformName := "android-$targetSdkVersion$",
+    // 署名に使用するエイリアス
+    keyalias := "$keyalias$",
+    // ProGuard を使用するか否か（true を強く推奨）
+    useProguard := true
   )
 
+  // ======================================================================
+  // 依存関係の解決に使用するリポジトリ
+  // ======================================================================
   val resolvers = Seq(
     "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository"
   )
 
+  // ======================================================================
+  // 依存関係
+  // ======================================================================
   val libraryDependencies = Seq(
+    
+    // Android Support Library v4 (jar)
     "android.support" % "compatibility-v4" % "19",
+    
+    // Android Support Library v7 (jar & apklib)
     "android.support" % "compatibility-v7-appcompat" % "19",
     apklib("android.support" % "compatibility-v7-appcompat" % "19")
+  
   )
 
+  // ======================================================================
+  // Scala コンパイラ起動オプション
+  // ======================================================================
   val scalacOptions = Seq("-feature")
 
+  // ======================================================================
+  // ProGuard 起動オプション
+  // ======================================================================
   val proguardOptions = Seq(
+    
+    // Scala のバグ？に対応するために必要……な場合もあるらしい。
+    // Issue -> https://issues.scala-lang.org/browse/SI-5397
+    // ビルドしたアプリケーションがうまく動かない場合はアンコメントを試す
+    //"-keep class scala.collection.SeqLike { public protected *;}",
+    
+    // Android Support Library v4 関連
     "-keep class android.support.v4.app.** { *; }",
     "-keep interface android.support.v4.app.** { *; }",
+    
+    // Android Support Library v7 関連
     "-keep class android.support.v7.app.** { *; }",
     "-keep interface android.support.v7.app.** { *; }",
     "-keep class android.support.v7.appcompat.** { *; }",
-    "-keep interface android.support.v7.appcompat.** { *; }",
-    "-keep class com.android.vending.billing.**",
-    /*
-       Since SI-5379 is not fixed yet, you need a below line.
-       See https://issues.scala-lang.org/browse/SI-5397 for more detail.
-     */
-    "-keep class scala.collection.SeqLike { public protected *;}"
+    "-keep interface android.support.v7.appcompat.** { *; }"
+  
   )
 
 }
